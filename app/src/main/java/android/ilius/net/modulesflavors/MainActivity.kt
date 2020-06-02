daa8e682e39564d17c281077550ab99832323c8a
TonyTandAndroid <tonythompsoncmu@gmail.com>
2020-06-01 20:07:14 -0700

    Add build config

--- app/src/main/java/android/ilius/net/modulesflavors/MainActivity.kt
+++ app/src/main/java/android/ilius/net/modulesflavors/MainActivity.kt
@@ -1,8 +1,8 @@
 package android.ilius.net.modulesflavors
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.lib.tracker.Constants
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<TextView>(R.id.tv_text).text = Constants.FLAVOR_CONFIGURED_IN_JAVA
+        findViewById<TextView>(R.id.tv_text).append(Constants.FLAVOR_CONFIGURED_IN_JAVA)
+        findViewById<TextView>(R.id.tv_text).append("\n")
+        findViewById<TextView>(R.id.tv_text).append(BuildConfig.SERVER_URL)
     }
 }
